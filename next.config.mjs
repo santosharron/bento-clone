90e5c93911c434be33c31615a2a4576bc78bcdca
Santosh Arron <73644573+santosharron@users.noreply.github.com>
2024-10-17 03:23:14 +0530

    Made Changes in Frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+      domains: ['res.cloudinary.com', 'www.google.com', 'randomuser.me'],
+    },
+  };
+  
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,59 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        xs: '440px',
+        xw: '300px',
+        xr: '580px',
+        xv: '520px',
+        xh: '390px',
+        'small-lg': '992px',
+      },
+      gridTemplateColumns: {
+        '1/2': '1.5fr 1fr',
+        auto: 'auto 1fr',
+      },
+      backgroundImage: {
+        primary:
+          'linear-gradient(to bottom right, black 0%, transparent 40%, transparent 60%, black 100%);',
+      },
+      boxShadow: {
+        logocard: '0 0 0 1px rgba(255, 255, 255, 0.12)',
+      },
+
+      animation: {
+        'fade-out1': 'fade-out1 40s linear infinite',
+        'fade-out2': 'fade-out2 40s linear infinite',
+      },
+
+      keyframes: {
+        'fade-out1': {
+          '0%': { transform: 'translateX(0%)' },
+
+          '50%': { transform: 'translateX(-100%)' },
+          '50.000001%': { transform: 'translateX(100%)' },
+
+          '100%': {
+            transform: 'translateX(0%)',
+          },
+        },
+        'fade-out2': {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-200%)',
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
 export default config;
