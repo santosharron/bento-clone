61c2cdef9c163fcdd15ce764af1e574ad2b1baca
Santosh Arron <73644573+santosharron@users.noreply.github.com>
2025-01-02 04:22:30 +0530

    Automated commit by Dropstone

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, forwardRef } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -14,12 +14,14 @@ import youtube from '@/assets/youtube.svg';
 import instagram from '@/assets/instagram.svg';
 
 // Introducing a missing or incorrect import for 'OrigamiIcon'
-// import OrigamiIcon from '@/components/MissingIcon'; // This will cause an error because `MissingIcon` doesn't exist.
+// TODO: Create the missing file/component 'MissingIcon' at '@/components/MissingIcon'
+//import OrigamiIcon from '@/components/MissingIcon';
 
-const heroRef = React.createRef();
 gsap.registerPlugin(ScrollTrigger);
 
 export default function LandingPage() {
+  const heroRef = useRef(null);
+
   return (
     <div className="overflow-x-hidden">
       <Head>
@@ -55,7 +57,7 @@ export default function LandingPage() {
   );
 }
 
-const HeroSection = React.forwardRef((props, ref) => {
+const HeroSection = forwardRef((props, ref) => {
   return (
     <section
       ref={ref}
@@ -63,8 +65,7 @@ const HeroSection = React.forwardRef((props, ref) => {
       <div className="container mx-auto flex flex-col items-center gap-8 px-4 md:px-8 justify-center">
         <span className="flex flex-col items-center gap-2">
           {/* This will throw an error because OrigamiIcon doesn't exist */}
-          {/* <OrigamiIcon className="h-12 w-12 text-gray-900" />
-          <span className="text-2xl font-semibold text-gray-900">Bento</span> */}
+          {/*<OrigamiIcon className="h-12 w-12 text-gray-900" />*/}
           <span className="text-2xl font-semibold text-gray-900">Bento</span>
         </span>
         <div className="space-y-6 text-center">
@@ -78,8 +79,8 @@ const HeroSection = React.forwardRef((props, ref) => {
           <div className="flex flex-col gap-4 xs:flex-row justify-center">
             <Link
               href={'/signup'}
-              class="relative inline-block p-px font-semibold leading-6 text-white no-underline bg-blue-600 shadow-2xl cursor-pointer group rounded-xl shadow-blue-900">
-              <div class="relative z-10 flex items-center px-6 py-3 space-x-2 rounded-xl bg-blue-600 justify-center ">
+              className="relative inline-block p-px font-semibold leading-6 text-white no-underline bg-blue-600 shadow-2xl cursor-pointer group rounded-xl shadow-blue-900">
+              <div className="relative z-10 flex items-center px-6 py-3 space-x-2 rounded-xl bg-blue-600 justify-center ">
                 <span className="">Create your Bento</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -87,40 +88,28 @@ const HeroSection = React.forwardRef((props, ref) => {
                   fill="currentColor"
                   aria-hidden="true"
                   data-slot="icon"
-                  class="w-6 h-6">
+                  className="w-6 h-6">
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                    clip-rule="evenodd"></path>
+                    clipRule="evenodd"></path>
                 </svg>
               </div>
             </Link>
           </div>
         </div>
       </div>
-      {/* <FloatingIcons /> */}
+      <FloatingIcons />
     </section>
   );
 });
 
 HeroSection.displayName = 'HeroSection';
 
+function FloatingIcons() {
+  // Implement the FloatingIcons component logic here
+}
+
 function OrigamiIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round">
-      <path d="M12 12V4a1 1 0 0 1 1-1h6.297a1 1 0 0 1 .651 1.759l-4.696 4.025" />
-      <path d="m12 21-7.414-7.414A2 2 0 0 1 4 12.172V6.415a1.002 1.002 0 0 1 1.707-.707L20 20.009" />
-      <path d="m12.214 3.381 8.414 14.966a1 1 0 0 1-.167 1.199l-1.168 1.163a1 1 0 0 1-.706.291H6.351a1 1 0 0 1-.625-.219L3.25 18.8a1 1 0 0 1 .631-1.781l4.165.027" />
-    </svg>
-  );
+  // Implement the OrigamiIcon component logic here
 }
