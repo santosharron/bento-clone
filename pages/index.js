1369e2abf973f4150717e8fd657ec1525ab6fa56
Santosh Arron <santosharron2906@gmail.com>
2025-01-09 11:13:54 +0000

    Automated commit by Dropstone

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,10 @@ import twitter from '@/assets/twitter.svg';
 import youtube from '@/assets/youtube.svg';
 import instagram from '@/assets/instagram.svg';
 
-// Introducing a missing or incorrect import for 'OrigamiIcon'
-import OrigamiIcon from '@/components/MissingIcon'; // This will cause an error because `MissingIcon` doesn't exist.
-
 gsap.registerPlugin(ScrollTrigger);
 
 export default function LandingPage() {
+  const heroRef = useRef(null);
   return (
     <div className="overflow-x-hidden">
       <Head>
@@ -76,8 +74,8 @@ const HeroSection = React.forwardRef((props, ref) => {
           <div className="flex flex-col gap-4 xs:flex-row justify-center">
             <Link
               href={'/signup'}
-              class="relative inline-block p-px font-semibold leading-6 text-white no-underline bg-blue-600 shadow-2xl cursor-pointer group rounded-xl shadow-blue-900">
-              <div class="relative z-10 flex items-center px-6 py-3 space-x-2 rounded-xl bg-blue-600 justify-center ">
+              className="relative inline-block p-px font-semibold leading-6 text-white no-underline bg-blue-600 shadow-2xl cursor-pointer group rounded-xl shadow-blue-900">
+              <div className="relative z-10 flex items-center px-6 py-3 space-x-2 rounded-xl bg-blue-600 justify-center ">
                 <span className="">Create your Bento</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -85,25 +83,25 @@ const HeroSection = React.forwardRef((props, ref) => {
                   fill="currentColor"
                   aria-hidden="true"
                   data-slot="icon"
-                  class="w-6 h-6">
+                  className="w-6 h-6">
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
-                    clip-rule="evenodd"></path>
+                    clipRule="evenodd"></path>
                 </svg>
               </div>
             </Link>
           </div>
         </div>
       </div>
-      <FloatingIcons />
+      {/* FloatingIcons component is not defined; comment out to avoid errors */}
+      {/* <FloatingIcons /> */}
     </section>
   );
 });
 
 HeroSection.displayName = 'HeroSection';
 
-// The error is triggered here
 function OrigamiIcon(props) {
   return (
     <svg
